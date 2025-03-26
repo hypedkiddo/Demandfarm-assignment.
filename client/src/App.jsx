3ed74db04e3e7f8098b054d19b0a030d27772b44
crazypunk08 <naren.lakamannavar@gmail.com>
2025-03-26 11:29:26 +0530

    Add PrivateRoute component for protected dashboard access and update routing in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import React,{useState,useEffect} from "react";
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route,Navigate,useNavigate } from 'react-router-dom';
 import Login from "./components/Login.jsx";
 import Register from "./components/Register.jsx";
 import LandingPage from "./components/LandingPage.jsx";
 import Dashboard from "./components/Dashboard.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import {auth} from "./firebase/firebase.js"
@@ -11,15 +12,15 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css"
 
 
-function Navigation() {
-  const navigate = useNavigate();
-  return (
-    <div>
-      <button onClick={() => navigate("/login")}>Login</button>
-      <button onClick={() => navigate("/register")}>Register</button>
-    </div>
-  );
-}
+// function Navigation() {
+//   const navigate = useNavigate();
+//   return (
+//     <div>
+//       <button onClick={() => navigate("/login")}>Login</button>
+//       <button onClick={() => navigate("/register")}>Register</button>
+//     </div>
+//   );
+// }
 
 function App() {
   const [user, setUser] = useState();
@@ -42,7 +43,7 @@ function App() {
               />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
-              <Route path="/dashboard" element={<Dashboard/>} /> 
+              <Route path="/dashboard" element={<PrivateRoute user={user}> <Dashboard/> </PrivateRoute>} /> 
             </Routes>
             <ToastContainer />
           </div>
